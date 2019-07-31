79bf448bbdcf396f0f7e5132a5c9fadb9f6299c1
Ashish Manmode <ash.manmode@gmail.com>
2019-07-31 11:33:31 +0530

    finished login flow

--- ekanna/js/login.js
+++ ekanna/js/login.js
@@ -1,33 +1,90 @@
+function show_otp_input()
+{
+	document.getElementById("otp_input").innerHTML = 
+	'<div class="input-field col s9"><input placeholder="123456" id="otp" type="text" class="validate" required><label for="otp">OTP</label></div>';
+}
 
 function login()
 {
 	var country = document.getElementById("country").value;
 	var phone_number = document.getElementById("phone_number").value;
+	var otp = document.getElementById("otp");
 
-	console.log(country);
-	console.log(phone_number);
+	var login_url = base_url + 'user/login';
+	
+	console.log(otp);
 
-    var cors = 'https://cors.io/?'
-	var login_url = cors + base_url + 'login';
-	var data =  {
-				  "phone_number": phone_number,
-				  "country_code": "IND"
-				}
+	if(otp == null){
+		var payload =  {
+					  "phone_number": phone_number,
+					  "country_code": "IND"
+					};
+		send_otp(login_url, payload)
+	}
+	else{
+		var payload =  {
+					  "phone_number": phone_number,
+					  "country_code": "IND",
+					  "otp":otp.value
+					};
+		verify_otp(login_url, payload)
+	}
 
-	$.post(login_url, data, function(data, status){
-        console.log("Data: " + data + "\nStatus: " + status);
-        var toastHTML = "";
-        if (status) {
-        	console.log('MAIL SENT');
-        	toastHTML = '<span>MAIL SENT SUCCESS</span><button class="btn-flat toast-action">OK</button>';
-        }
-        else
-        {
-        	console.log('FAILED! Please try again.');
-        	toastHTML = '<span>FAIL: Try Again</span><button class="btn-flat toast-action">OK</button>';
-        }
-        M.toast({html: toastHTML});
-    });
+	
+}
+
+function send_otp(login_url, payload) {
+
+	var response = document.getElementById('login_responce');
+	response.innerHTML = ''
+
+	$.ajax({
+	  url:login_url,
+	  type:"POST",
+	  data:JSON.stringify(payload),
+	  contentType:"application/json; charset=utf-8",
+	  dataType:"json",
+	  success: function(result,status,xhr){
+	  	show_otp_input();
+	    console.log(xhr.responseJSON);
+	    response.innerHTML = '<font color="green">'+
+	  	                     xhr.responseJSON['message']
+	  	                     +'</font>';
+	  	document.getElementById('test_otp').innerHTML = xhr.responseJSON['otp'];
+	  },
+	  error: function(xhr,status,error){
+	  	response.innerHTML = '<font color="red">'+
+	  	                     xhr.responseJSON['message']
+	  	                     +'</font>';
+	  }
+	})
+}
+
+function verify_otp(login_url, payload) {
+
+	var response = document.getElementById('login_responce');
+	response.innerHTML = ''
+	document.getElementById('test_otp').innerHTML = '';
+
+	$.ajax({
+	  url:login_url,
+	  type:"POST",
+	  data:JSON.stringify(payload),
+	  contentType:"application/json; charset=utf-8",
+	  dataType:"json",
+	  success: function(result,status,xhr){
+	    console.log(xhr.responseJSON);
+	    response.innerHTML = '<font color="green">'+
+	  	                     xhr.responseJSON['message']
+	  	                     +'</font>';
+	  	document.getElementById('test_otp').innerHTML = xhr.responseJSON['authorization'];
+	  },
+	  error: function(xhr,status,error){
+	  	response.innerHTML = '<font color="red">'+
+	  	                     xhr.responseJSON['message']
+	  	                     +'</font>';
+	  }
+	})
 }
 
 
